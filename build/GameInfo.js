8ce69d7be953ee3de54a9643b66da3951d211530
PieterGeschiere <gesc0018@hz.nl>
2021-12-20 11:26:14 +0100

    player speed
    fixed player speed now its better

--- build/GameInfo.js
+++ build/GameInfo.js
@@ -1,7 +1,7 @@
 export default class GameInfo {
-    static PLAYER_Y_SPEED = 6;
+    static PLAYER_Y_SPEED = 9;
     static PLAYER_X_SPEED = 6;
-    static GRAVITY_CONSTANT = 0.0098;
+    static GRAVITY_CONSTANT = 0.098;
     static PLAYER_AIRBORNE_X_SPEED_PENTALTY = 2;
 }
 //# sourceMappingURL=GameInfo.js.map
