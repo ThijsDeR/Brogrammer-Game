ac3be0e0bbdd1dd9b8bb875666bba3d1c537cb0d
ThijsDeR <rech0004@hz.nl>
2022-01-13 12:52:23 +0100

    removed build

--- build/Doodle/DoodleLevelInfo.js
+++
@@ -1,7 +0,0 @@
-export default class DoodleLevelInfo {
-    static PLAYER_Y_SPEED = 3;
-    static PLAYER_X_SPEED = 9;
-    static GRAVITY_CONSTANT = 0.0098;
-    static PLAYER_AIRBORNE_X_SPEED_PENTALTY = 2;
-}
-//# sourceMappingURL=DoodleLevelInfo.js.map
--- build/Hub/HubPlayer.js
+++
@@ -1,8 +0,0 @@
-import Player from '../Player.js';
-export default class HubPlayer extends Player {
-    props;
-    constructor(xPos, yPos, width = undefined, height = undefined) {
-        super(xPos, yPos, './assets/img/Dood.jpg', width, height);
-    }
-}
-//# sourceMappingURL=HubPlayer.js.map
