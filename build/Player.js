debd2f0e3de15168ac5a48de2ae359c9efa7fb5f
Jochem Dijkdrent <dijk0146@hz.nl>
2022-10-31 12:08:35 +0100

    mooier gemaakt van code
    @ThijsDeR mooie melding dit ee

--- build/Player.js
+++ build/Player.js
@@ -32,7 +32,8 @@ export default class Player extends ImageProp {
         this.xVel = 0;
         if (!this.airborne) {
             if (this.keyboardListener.isKeyDown(KeyboardListener.KEY_SPACE)
-                || this.keyboardListener.isKeyDown(KeyboardListener.KEY_UP) || this.keyboardListener.isKeyDown(KeyboardListener.KEY_W)) {
+                || this.keyboardListener.isKeyDown(KeyboardListener.KEY_UP) 
+                || this.keyboardListener.isKeyDown(KeyboardListener.KEY_W)) {
                 this.yVel = -(GameInfo.PLAYER_Y_SPEED) * (this.height / 100);
             }
         }
